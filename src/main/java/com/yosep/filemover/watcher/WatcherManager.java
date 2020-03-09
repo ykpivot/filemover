aa9889509b7db362c7f52a7516b4e306b075965b
Yosep Kim <yosepk@gmail.com>
2020-03-09 18:35:21 -0400

    makes the FileManager test pass

--- src/main/java/com/yosep/filemover/watcher/WatcherManager.java
+++ src/main/java/com/yosep/filemover/watcher/WatcherManager.java
@@ -1,10 +1,20 @@
 package com.yosep.filemover.watcher;
 
+import java.util.List;
+
 public class WatcherManager {
+    private WatcherFactory watcherFactory;
+
     public WatcherManager(WatcherFactory watcherFactory) {
-        
+
+        this.watcherFactory = watcherFactory;
     }
 
     public void start() {
+        List<FileWatcher> watchers = this.watcherFactory.create();
+
+        for (FileWatcher watcher : watchers) {
+            watcher.start();
+        }
     }
 }
