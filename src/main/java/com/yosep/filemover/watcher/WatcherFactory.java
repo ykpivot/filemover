2e518e86328cc02c7bcce04bc10b675d85a3e2ac
Yosep Kim <yosepk@gmail.com>
2020-03-09 18:23:59 -0400

    made the WatcherFactory test pass

--- src/main/java/com/yosep/filemover/watcher/WatcherFactory.java
+++ src/main/java/com/yosep/filemover/watcher/WatcherFactory.java
@@ -1,12 +1,24 @@
 package com.yosep.filemover.watcher;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WatcherFactory {
+    private BankService bankService;
+    private MonitorConfig monitorConfig;
+
     public WatcherFactory(BankService bankService, MonitorConfig monitorConfig) {
+        this.bankService = bankService;
+        this.monitorConfig = monitorConfig;
     }
 
     public List<FileWatcher> create() {
-        return null;
+        List<FileWatcher> watchers = new ArrayList<>();
+
+        for (Bank bank : this.bankService.getBanks()) {
+            FileWatcher watcher = new FileWatcher(bank.getBankId(), this.monitorConfig.isDoNotProcessLargeFile());
+            watchers.add(watcher);
+        }
+        return watchers;
     }
 }
